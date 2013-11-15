b0b8989f786d2e39dd16f6c5f52fb5ef1eedccc5
Sasha <dimurchik@ukr.net>
2013-11-15 21:11:53 +0200

    write decorator ifThisChanged and limited

--- homework6.js
+++ homework6.js
@@ -2,7 +2,49 @@
  * Created by sasha on 15.11.13.
  */
 ;!function(){
+	window.ifThisChanged = function(fieldName, originalFunction){
 
+		var field,
+			newThis,
+			newArguments;
+
+		return function(){
+			newThis = this;
+			newArguments = arguments;
+			if (field == this[fieldName]) return;
+			field = this[fieldName];
+			return originalFunction.call(newThis, arguments);
+		};
+	}
+
+	window.limited = function limited(time, originalFunction ){
+
+		if (typeof time === 'function'){
+			originalFunction = time;
+			time = 20;
+		}
+		var isTimerRunning=false,
+				newThis,
+				newArguments;
+
+		return function(){
+			newThis = this;
+			newArguments = arguments;
+
+			if (isTimerRunning) return;
+
+			isTimerRunning = true;
+			setTimeout(function(){
+
+						isTimerRunning = false;
+						return originalFunction.apply(newThis, newArguments);
+					},
+					time
+			);
+
+
+		};
+	}
 }();
 
 
@@ -20,3 +62,9 @@ obj.test('third');
 obj.field = 2;
 
 obj.test('fourth'); // fourth
+
+document
+		.getElementById('test')
+		.addEventListener('keyup', ifThisChanged('value', limited(500, function(){
+console.log(this.value);
+})));
