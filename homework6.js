bedc88d46001d81917bee464aa3d00d2705cbd28
Sasha <dimurchik@ukr.net>
2013-11-15 21:17:24 +0200

    refactoring code

--- homework6.js
+++ homework6.js
@@ -1,14 +1,16 @@
 /**
  * Created by sasha on 15.11.13.
  */
-;!function(){
-	window.ifThisChanged = function(fieldName, originalFunction){
+;
+!function(){
+
+	window.ifThisChanged = function(fieldName, originalFunction) {
 
 		var field,
-			newThis,
-			newArguments;
+				newThis,
+				newArguments;
 
-		return function(){
+		return function() {
 			newThis = this;
 			newArguments = arguments;
 			if (field == this[fieldName]) return;
@@ -17,42 +19,39 @@
 		};
 	}
 
-	window.limited = function limited(time, originalFunction ){
+	window.limited = function limited(time, originalFunction) {
 
-		if (typeof time === 'function'){
+		if (typeof time === 'function') {
 			originalFunction = time;
 			time = 20;
 		}
-		var isTimerRunning=false,
+		var isTimerRunning = false,
 				newThis,
 				newArguments;
 
-		return function(){
+		return function() {
 			newThis = this;
 			newArguments = arguments;
 
 			if (isTimerRunning) return;
 
 			isTimerRunning = true;
-			setTimeout(function(){
-
+			setTimeout(function() {
 						isTimerRunning = false;
 						return originalFunction.apply(newThis, newArguments);
 					},
 					time
 			);
-
-
 		};
 	}
 }();
 
 
-
-
 var obj = {
 	field: 1,
-	test: ifThisChanged('field', function(name){ console.log(name) })
+	test: ifThisChanged('field', function(name) {
+		console.log(name)
+	})
 };
 
 obj.test('first'); // first
@@ -65,6 +64,6 @@ obj.test('fourth'); // fourth
 
 document
 		.getElementById('test')
-		.addEventListener('keyup', ifThisChanged('value', limited(500, function(){
-console.log(this.value);
-})));
+		.addEventListener('keyup', ifThisChanged('value', limited(500, function() {
+			console.log(this.value);
+		})));
